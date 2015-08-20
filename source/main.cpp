797fafe63394b8e1a087406a1925ab2d649fe386
John Beck <john.beck@metova.com>
2015-08-20 15:41:34 -0500

    nit pickin

--- source/main.cpp
+++ source/main.cpp
@@ -7,14 +7,13 @@
 // * last modified: 2013-03-30
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-
 #include "Longshot.h"
 
 int main(int argc, char *argv[]);
 
 // program entry point
 int main(int argc, char *argv[]) {
-  Longshot *longshot;
+  Longshot* longshot;
   longshot = new Longshot;
   int value = longshot->loop();
   delete longshot;
